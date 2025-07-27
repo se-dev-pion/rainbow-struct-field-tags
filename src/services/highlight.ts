0be2eabb1985e65c1ca7458e0c122bac4d2e4d66
BioCrossCoder <viscaria_dignics@163.com>
2025-07-27 09:47:55 +0800

    feat: add new config item

--- src/services/highlight.ts
+++ src/services/highlight.ts
@@ -1,11 +1,22 @@
 import vscode from 'vscode';
-import { configValueItemColor, configKey, configKeyColor, configValueOptionColor, configValueGapColor, configTextColor, configBackgroundColor, configGormTagKey } from '../common/constants';
+import {
+    configValueItemColor,
+    configKey,
+    configKeyColor,
+    configValueOptionColor,
+    configValueGapColor,
+    configTextColor,
+    configBackgroundColor,
+    configGormTagKey,
+    configOptionBranchColor
+} from '../common/constants';
 import { debounced, getCurrentEditor, isSupportedLanguage } from '../common/utils';
 import { divideDecoratedBlocks } from '../logics/decorate';
 
 let keyStyle: vscode.TextEditorDecorationType;
 let itemStyle: vscode.TextEditorDecorationType;
 let optionStyle: vscode.TextEditorDecorationType;
+let branchStyle: vscode.TextEditorDecorationType;
 let gapColor: vscode.TextEditorDecorationType;
 let tagStyle: vscode.TextEditorDecorationType;
 let gormTagKey: string;
@@ -16,12 +27,14 @@ export function addHighlight() {
         if (!isSupportedLanguage(editor.document.languageId)) {
             return;
         } // [/]
-        const { tagRanges, keyRanges, itemRanges, optionRanges, gapRanges } = divideDecoratedBlocks(editor.document, gormTagKey);
+        const { tagRanges, keyRanges, itemRanges, optionRanges, branchRanges, gapRanges } =
+            divideDecoratedBlocks(editor.document, gormTagKey);
         // [ApplyDecorations]
         editor.setDecorations(tagStyle, tagRanges);
         editor.setDecorations(keyStyle, keyRanges);
         editor.setDecorations(itemStyle, itemRanges);
         editor.setDecorations(optionStyle, optionRanges);
+        editor.setDecorations(branchStyle, branchRanges);
         editor.setDecorations(gapColor, gapRanges); // [/]
     }, 50);
     loadConfigs();
@@ -42,19 +55,24 @@ function loadConfigs() {
     gormTagKey = vscode.workspace.getConfiguration(configKey).get(configGormTagKey) as string;
     // The style created earlier has higher priority
     keyStyle = vscode.window.createTextEditorDecorationType({
-        color: vscode.workspace.getConfiguration(configKey).get(configKeyColor) as string,
+        color: vscode.workspace.getConfiguration(configKey).get(configKeyColor) as string
     });
     itemStyle = vscode.window.createTextEditorDecorationType({
-        color: vscode.workspace.getConfiguration(configKey).get(configValueItemColor) as string,
+        color: vscode.workspace.getConfiguration(configKey).get(configValueItemColor) as string
     });
     optionStyle = vscode.window.createTextEditorDecorationType({
-        color: vscode.workspace.getConfiguration(configKey).get(configValueOptionColor) as string,
+        color: vscode.workspace.getConfiguration(configKey).get(configValueOptionColor) as string
+    });
+    branchStyle = vscode.window.createTextEditorDecorationType({
+        color: vscode.workspace.getConfiguration(configKey).get(configOptionBranchColor)
     });
     gapColor = vscode.window.createTextEditorDecorationType({
-        color: vscode.workspace.getConfiguration(configKey).get(configValueGapColor) as string,
+        color: vscode.workspace.getConfiguration(configKey).get(configValueGapColor) as string
     });
     tagStyle = vscode.window.createTextEditorDecorationType({
         color: vscode.workspace.getConfiguration(configKey).get(configTextColor),
-        backgroundColor: vscode.workspace.getConfiguration(configKey).get(configBackgroundColor) as string,
+        backgroundColor: vscode.workspace
+            .getConfiguration(configKey)
+            .get(configBackgroundColor) as string
     });
 }
