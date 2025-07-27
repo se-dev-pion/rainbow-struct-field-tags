66cc8dad9ce70f9810689950c37cb56fd63d9b12
BioCrossCoder <viscaria_dignics@163.com>
2025-07-27 09:47:55 +0800

    feat: add new config item

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -8,6 +8,7 @@ export const keyValueSeparator = 'key_value';
 export const valueItemsSeparator = 'value_items';
 export const valueBorder = 'value_border';
 export const itemOptionSeparator = 'item_option';
+export const optionBranchSeparator = 'option_branch';
 export const tagBorder = 'tag_border';
 export const separators = {
     [keyValueSeparator]: ':',
@@ -18,11 +19,12 @@ export const separators = {
 } as const; // [/]
 
 // [ConfigurationKeys]
-export const configKey = "rainbow-struct-field-tags";
-export const configKeyColor = "key-color";
-export const configValueItemColor = "value-item-color";
-export const configValueOptionColor = "value-option-color";
-export const configValueGapColor = "value-gap-color";
+export const configKey = 'rainbow-struct-field-tags';
+export const configKeyColor = 'key-color';
+export const configValueItemColor = 'value-item-color';
+export const configValueOptionColor = 'value-option-color';
+export const configOptionBranchColor = 'option-branch-color';
+export const configValueGapColor = 'value-gap-color';
 export const configBackgroundColor = 'background-color';
 export const configTextColor = 'text-color';
 export const configGormTagKey = 'gorm-tag-key'; // [/]
@@ -36,6 +38,8 @@ export const singleLineStringSign = '"'; // [/]
 // [GormRelatedConstants]
 export const gormSeparators = {
     [valueItemsSeparator]: ';',
-    [itemOptionSeparator]: ':'
+    [itemOptionSeparator]: ':',
+    [optionBranchSeparator]: ','
 } as const;
-export const gormTagItemComment = 'comment' + gormSeparators[itemOptionSeparator]; // [/]
+export const gormTagItemComment = 'comment' + gormSeparators[itemOptionSeparator];
+export const gormIndexTagKeys = ['index', 'uniqueIndex']; // [/]
